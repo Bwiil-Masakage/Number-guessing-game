0be11006386493e5e9cfa671c47d5612f2a1788d
Godswill Bassey <godswillbasseyflash@gmail.com>
2022-10-17 09:13:41 +0100

    Update Game code.py

--- Game code.py
+++ Game code.py
@@ -1,9 +1,9 @@
 import random
 number = random.randint(1, 10)
 
-player_name = input("Hello, what is your name? ")
+player_name = input("Hello there, you have a name? ")
 number_of_guesses = 0
-print('I\'m glad to meet you! {} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:'.format(player_name))
+print('It\'s! a wonderful day to meet you! {} \nLet\'s play a game together, I\'m gonna pick number between 1 and 10 in my mind, then you will guess which number it is, ready? \nDon\'t forget! You only have 3 chances to guess.\n GOODLUCK!:'.format(player_name))
 
 while number_of_guesses < 3:
     guess = int(input())
@@ -15,6 +15,6 @@ while number_of_guesses < 3:
     if guess == number:
         break
 if guess == number:
-    print( 'Congratulations {}, you guessed the number in {} tries!'.format(player_name, number_of_guesses))
+    print( 'Congratulations {}, you guessed the number right in {} tries!'.format(player_name, number_of_guesses))
 else:
-    print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was {}.'.format(number))
+    print('Oh snap! You couldn\'t guess the number. \nAnyway, the number was {}. Try again!'.format(number))
