db283af9d3e2e5bb4daec34882dcec58d11e788c
Godswill Bassey <godswillbasseyflash@gmail.com>
2022-10-23 22:48:34 +0100

    Update Game code.py

--- Game code.py
+++ Game code.py
@@ -1,9 +1,17 @@
 import random
+import time
+
+print('GUESS!')
+time.sleep(2)
+print('THE!')
+time.sleep(2)
+print('NUMBER!')
+
 number = random.randint(1, 10)
 
 player_name = input("Hello there, you have a name? ")
 number_of_guesses = 0
-print('It\'s! a wonderful day to meet you! {} \nLet\'s play a game together, I\'m gonna pick number between 1 and 10 in my mind, then you will guess which number it is, ready? \nDon\'t forget! You only have 3 chances to guess.\n GOODLUCK!:'.format(player_name))
+print('\n It\'s a wonderful day to meet you! {} \n\nLet\'s play a game together, I\'m gonna pick a number between 1 and 10 in my mind, then you will guess which number it is, ready? \n \nDon\'t forget! You only have 3 chances to guess.\n\n GOODLUCK!:'.format(player_name))
 
 while number_of_guesses < 3:
     guess = int(input())
